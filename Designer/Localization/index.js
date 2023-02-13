7032f05acce7b5495fd302760a819cdeb44511b3
optimajet <support@optimajet.com>
2023-02-13 15:55:28 +0100

    WFE 7.3.0 (autocommit)

---
+++ Designer/Localization/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  en: require('./en_default.json'),
+  de: require('./de.json'),
+  es: require('./es.json'),
+  fr: require('./fr.json'),
+  it: require('./it.json'),
+  pt: require('./pt.json'),
+  ru: require('./ru.json'),
+  tr: require('./tr.json'),
+  ar: require('./ar.json')
+};
