3ba7eb952ae650e9a2903e3409a0275c7e58c2de
optimajet <support@optimajet.com>
2023-04-20 19:38:02 +0200

    WFE 9.0.0 (autocommit)

--- Samples/ASP.NET MVC/Oracle/WF.Sample/Models/IPaging.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WF.Sample.Models
-{
-    public interface IPaging
-    {
-        int Count { get; set; }
-        int Page { get; set; }
-        int PageSize { get; set; }
-    }
-}
--- Samples/ASP.NET WebForms/WebFormsOracle/WF.Sample/Pages/Document/DeleteRowsHandler.ashx
+++
@@ -1,1 +0,0 @@
-﻿<%@ WebHandler Language="C#" CodeBehind="DeleteRowsHandler.ashx.cs" Class="WF.Sample.Document.DeleteRowsHandler" %>
